d17783144a78b0c095f274c1174bba6d571a5751
Bhagyashri271998 <59722604+Bhagyashri271998@users.noreply.github.com>
2021-06-09 15:15:04 +0530

    done

---
+++ OnlineApp/server.js
@@ -0,0 +1,38 @@
+/*var count=34;
+count++;
+console.log("Server is running");
+console.log(count);
+
+
+//CTL + SHIFT + C   
+
+*/
+
+var express =require("express");  //lib-----Node Module
+var app=express();  // will create app object from express package
+
+var onAboutUs=function(req, res){
+    res.send("Chief Mentor :Bhagyashri Gawas");
+};
+var onDefault=function(req, res){
+    res.send("<h1>IACSD pune</h1>"+
+              "<hr/>"+
+	      "<h2>Courses</h2>"+
+              "<ol>"+
+                    " <li>e-DBDA</li>"+
+                    " <li>e-DAC</li>"+
+                    " <li>e-DITISS</li>"+
+             "</ol>");
+};
+app.get("/",onDefault);  // Request handler functions are registered
+app.get("/aboutus",onAboutUs);  // Request handler functions are registered
+var server=app.listen(8080);
+console.log("Server is running on port 8081");
+// if external packages are required
+// we have to download those packages from internet using  package manager tool
+// Runtime--------tools-------------packages-----------------------config file
+// Java-----------maven--------------.jar files----------------------pom.xml
+// Android--------graddle------------android jar file----------------pom.xml
+// Python---------pip----------------Python libraries----------------
+// .NET ---------Nugget--------------dll files-----------------------package.config
+// NodeJS----------npm --------------download node modules-----------package.json 
